9230909ba1943b3b313f7f7646b9fd519e483cf2
Santiago Duque <santiago.duque.chacon@correounivalle.edu.co>
2023-01-20 00:11:45 -0500

    ULtimo commit

--- src/modelo/Formulas.java
+++ src/modelo/Formulas.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
+
+import java.util.Map;
+import java.util.TreeMap;
 import modelo.Coeficientes;
 
 /**
@@ -10,5 +13,35 @@ import modelo.Coeficientes;
  * @author gurui
  */
 public class Formulas {
+
     protected static Coeficientes coeficientesActuales;
+
+    public Coeficientes MinimosCuadrados(TreeMap<Integer, Float> mapaVentas) {
+        
+        int n = mapaVentas.size();
+        double[] x = new double[n];
+        double[] y = new double[n];
+        int i = 0;
+        //  Iterando sobre el treemap
+        for (Map.Entry<Integer, Float> entry : mapaVentas.entrySet()) {
+            x[i] = entry.getKey();
+            y[i] = entry.getValue();
+            i++;
+        }
+        //  Calcular las sumas necesarias para minimos cuadrados
+        double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+        for (i = 0; i < n; i++) {
+            sumX += x[i];
+            sumY += y[i];
+            sumXY += x[i] * y[i];
+            sumX2 += x[i] * x[i];
+        }
+        //  Calcular pendiente b y el intercepto a
+        double b= (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+        double a = (sumY - b * sumX) / n;
+        
+        Coeficientes coeficientesObt = new Coeficientes(a, b);
+        
+        return coeficientesObt;
+    }
 }
